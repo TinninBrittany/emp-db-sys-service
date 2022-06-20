9bce80c38076cad173feb26ab5ecaff256ea6eeb
Brittany Tinnin <bntinnin7@gmail.com>
2022-06-20 12:32:29 -0700

    add employee list functionality

---
+++ src/main/java/com/employeedatabasesystem/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.employeedatabasesystem.controller;
+
+import com.employeedatabasesystem.domain.Employee;
+import com.employeedatabasesystem.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
---
+++ src/main/java/com/employeedatabasesystem/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.employeedatabasesystem.service;
+
+import com.employeedatabasesystem.domain.Employee;
+import com.employeedatabasesystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+}
