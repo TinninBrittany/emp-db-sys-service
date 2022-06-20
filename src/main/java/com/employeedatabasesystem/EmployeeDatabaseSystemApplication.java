3ecb3e5a3da4746a67ecaae2e4272fa8f4823a54
Brittany Tinnin <bntinnin7@gmail.com>
2022-06-20 09:17:11 -0700

    first commit

---
+++ src/main/java/com/employeedatabasesystem/EmployeeDatabaseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.employeedatabasesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDatabaseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDatabaseSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeedatabasesystem/EmployeeDatabaseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeedatabasesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDatabaseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
